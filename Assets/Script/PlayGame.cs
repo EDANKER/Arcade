d5290347b49b6d6d83e96dd1ade8bc6c87635044
Edanker <eledyall@bk.ru>
2023-09-06 01:10:24 +0500

    MenuPanel And DownloadScene

--- Assets/Script/PlayGame.cs
+++ Assets/Script/PlayGame.cs
@@ -1,8 +1,11 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayGame : MonoBehaviour
 {
+    [SerializeField] private Animator _animator;
+
     public void ButtonPlay()
     {
         SceneManager.LoadScene(1);
